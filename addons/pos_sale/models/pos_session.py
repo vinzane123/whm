b42314e62b64ec8462cb2952a0e415fbcb1ecd2a
vinodhp <vinodhpolamarasetty@gmail.com>
2020-01-17 20:34:18 +0530

    check

--- addons/pos_sale/models/pos_session.py
+++ addons/pos_sale/models/pos_session.py
@@ -8,3 +8,5 @@ class PosSession(models.Model):
     _inherit = 'pos.session'
 
     crm_team_id = fields.Many2one('crm.team', related='config_id.crm_team_id', string="Sales Team", readonly=True)
+
+#trying to add and commit.
